8d69017aefceff3c762a58f5a925ff65bbe42b86
psychoCS <thiagoluizb@hotmail.com>
2020-09-25 16:18:15 -0400

    finished the new version

--- server2.js
+++ server2.js
@@ -1,11 +1,24 @@
 const connect = require('connect');
 const app = connect();
 
-function helloWorld(req, res, next) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+function logger(req, res, next) {
+    console.log(req.method, req.url);
+    next();
 };
 
-app.use(helloWorld);
+function helloWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello world')
+}
+
+function goodbyeWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Goodbye world')
+}
+
+app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbyeWorld);
+
 app.listen(3000);
 console.log('Server running at http://localhost:3000/');
