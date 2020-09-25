fc3c9a06983ecb5da623396f0d9d3727a33f790e
psychoCS <thiagoluizb@hotmail.com>
2020-09-25 16:53:01 -0400

    Success on nodemon

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const http = require("http");
 http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-type": "text/plain" });
-    res.end("testing... 1...2...3");
-  })
-  .listen(3000);
+    .createServer((req, res) => {
+        res.writeHead(200, {
+            "Content-type": "text/plain"
+        });
+        res.end("Lets try");
+    })
+    .listen(3000);
 
 console.log("Server running at http://localhost:3000/");
+
+/*
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});*/
